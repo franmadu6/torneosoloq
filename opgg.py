e40d8b86c5581d16a0d3a1baa0f54d30089d522b
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2020-06-12 01:29:56 +0200

    Update opgg.py

--- opgg.py
+++ opgg.py
@@ -7,8 +7,8 @@ from flask import Flask, redirect, render_template, request, session, url_for,ab
 import os
 
 server = "euw"
-players = [{"name": "cuenta1","account": "TitoMadu"},{"name": "cuenta4","account":"Madu on Bush"},{"name": "cuenta2","account": "AKISAMA"},
-	   {"name": "cuenta3","account": "rorozk8"}]
+players = [{"name": "Madueño","account": "Koon Eduan"},{"name": "Manu Sanchez","account":"EseOGThueleaPDT"},{"name": "Juaki","account": "ElMessiDelRoneo"},
+	   {"name": "Pae","account": "AlanBritoDelgado"},{"name": "Juanca","account": "Titojuankaisback"},{"name": "Ale R","account": "SQCurry"}]
 ################################################################################
 ranks = ["Challenger","Grandmaster","Master","Diamond 1","Diamond 2","Diamond 3","Diamond 4","Platinum 1","Platinum 2","Platinum 3","Platinum 4","Gold 1","Gold 2","Gold 3","Gold 4","Silver 1","Silver 2","Silver 3","Silver 4","Bronze 1","Bronze 2","Bronze 3","Bronze 4","Iron 1","Iron 2","Iron 3", "Iron 4","Unranked"]
 
